584f8b31232e78f3c667d8545ab450514fdd5ec0
Christopher Magnuson <cmagnuson0489@gmail.com>
2024-07-19 12:56:16 -0700

    Create App.jsx
    Created App.jsx for front end. Iniital set up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import UserList from './components/UserProfile';
+import Chat from './components/Chat';
+import { AuthProvider } from './context/AuthContext';
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <Switch>
+            <Route path="/login" component={login} />
+            <Route path="/users" component={UserList} />
+            <Route path ="/profile" component={UserProfile} />
+            <Route path="/chat" component={Chat} />
+        </Switch>
+      </Router>
+    </AuthProvider>
+  );
+};
